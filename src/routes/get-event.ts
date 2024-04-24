e1d3b4fd7e9bbb60a7f6ab56127378ebc09b42ef
PedrOliveiraM <pedropucmont@gmail.com>
2024-04-23 22:53:21 -0300

    feat: Add new route getEvent for return data from an event

---
+++ src/routes/get-event.ts
@@ -0,0 +1,70 @@
+import { FastifyInstance } from "fastify";
+import { z } from "zod";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import { prisma } from "../libs/prisma";
+import { title } from "process";
+
+export async function getEvent(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().get(
+    "/events/:eventId",
+    {
+      schema: {
+        params: z.object({
+          //pegando o id do evento que ta na url
+          eventId: z.string().uuid(), //validando se o id é um uuid
+        }),
+        response: {
+          200: z.object({
+            event: z.object({
+              id: z.string().uuid(),
+              title: z.string(),
+              details: z.string(),
+              slug: z.string(),
+              maximumAttendees: z.number(),
+              attendees: z.number(),
+            }),
+          }),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { eventId } = request.params; //pegando o id do evento que ta na url
+
+      // procurando o evento pelo id no banco de dados
+      const event = await prisma.event.findUnique({
+        select: {
+          //quais colnas eu quero pegar
+          id: true,
+          title: true,
+          details: true,
+          slug: true,
+          maximumAttendees: true,
+          _count: {
+            select: {
+              attendees: true,
+            },
+          },
+        },
+        where: {
+          id: eventId,
+        },
+      });
+
+      //se evento for null ele retorna um erro de nao encontrado
+      if (event === null) {
+        throw new Error("Event not found event");
+      }
+
+      return reply.send({
+        event: {
+          id: event.id,
+          title: event.title,
+          details: event.details,
+          slug: event.slug,
+          maximumAttendees: event.maximumAttendees,
+          attendees: event._count.attendees,
+        },
+      }); // retornando o evento em forma de objeto
+    }
+  );
+}
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,16 @@
-import { create } from "domain";
 import fastify from "fastify";
-import { serializerCompiler, validatorCompiler, ZodTypeProvider } from "fastify-type-provider-zod";
+import { serializerCompiler, validatorCompiler} from "fastify-type-provider-zod";
 import { createEvent } from "./routes/create-event";
 import { registerForEvent } from "./routes/register-for-event";
+import { getEvent } from "./routes/get-event";
 const app = fastify();
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
-app.register(createEvent); // 
-app.register(registerForEvent);
+app.register(createEvent); // rota para criar o evento
+app.register(registerForEvent); // rota para registrar um evento
+app.register(getEvent); // rota para pegar os dados do evento
 
 app.listen({ port: 3333 }).then(() => {
   console.log("HTTP server is running on port 3333");
